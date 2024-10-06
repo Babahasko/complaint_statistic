910db18ebf7e821640bb4fa8dc10e0fc978a0cf2
Babahasko <zakutni4ek@gmail.com>
2024-10-06 23:12:30 +0300

    addd naming convention to metadata

--- complaint_service/model.py
+++ complaint_service/model.py
@@ -2,8 +2,11 @@ from sqlalchemy import Table, Column, Integer, String
 from sqlalchemy.orm import registry
 from sqlalchemy import MetaData
 from .schema import Complain
+from .config import settings
+from sqlalchemy import UniqueConstraint
 
-mapper_registry = registry()
+metadata_obj = MetaData(naming_convention=settings.db.naming_convention)
+mapper_registry = registry(metadata=metadata_obj)
 
 complain_table = Table(
     "complain_table",
@@ -14,4 +17,5 @@ complain_table = Table(
     Column("about", String(30)),
     Column("data", String(30)),
 )
+
 mapper_registry.map_imperatively(Complain, complain_table)
