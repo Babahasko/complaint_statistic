95bfd78cedd53c2a363ffed11b60bebd2bf165ad
Babahasko <zakutni4ek@gmail.com>
2024-10-09 19:22:20 +0300

    last working test

--- test_crud.py
+++ test_crud.py
@@ -2,24 +2,36 @@ import pytest
 from complaint_service.schema import Complain
 from complaint_service.db_helper import db_helper
 from complaint_service.repository import ComplainRepository
+from complaint_service.logger import logger
 from datetime import datetime
 
-from loguru import logger
-import asyncio
-import sys
-
-logger.remove()
-logger.add(sys.stderr,enqueue=True, level = 'DEBUG')
 
 @pytest.mark.asyncio
-async def test_writing_in_db():
+async def test_writing_and_reding_from_db():
+    logger.info('Starting testing creating')
     time = datetime.now()
     complain = Complain(who='Дед', whom='Мирон', about='Колени', data=time)
     async_generator = db_helper.session_getter()
     async_session = await anext(async_generator)
-    result = await ComplainRepository.add_complain(session=async_session, complain=complain)
-    print(result)
+    logger.info(f'async_session = {async_session.__dict__}')
+    logger.info(f'complain = {complain}')
+    await ComplainRepository.create_complain(session=async_session, complain=complain)
     await db_helper.dispose()
 
-    assert result == complain
+@pytest.mark.asyncio
+async def test_reading_from_db():
+    logger.info('Starting testing reading')
+    async_generator = db_helper.session_getter()
+    async_session = await anext(async_generator)
+    result = await ComplainRepository.select_all_complaints(session=async_session)
+    logger.info(f'result_reading_from_db = {result}')
+    await db_helper.dispose()
 
+# @pytest.mark.asyncio
+# async def test_deleting_from_db():
+#     logger.info('Start testing deleting')
+#     async_generator = db_helper.session_getter()
+#     async_session = await anext(async_generator)
+#     result = await ComplainRepository.delete_complain(session=async_session)
+#     logger.info(f'result_deleting_from_db = {result}')
+#     await db_helper.dispose()
