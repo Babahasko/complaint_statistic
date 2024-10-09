f82c875835a885222e34115d79b0a860ec3272bc
Babahasko <zakutni4ek@gmail.com>
2024-10-10 00:22:24 +0300

    it don`t work. i don`t know why

--- conftest.py
+++ conftest.py
@@ -2,9 +2,13 @@ import pytest
 from complaint_service.db_helper import DatabaseHelper
 from complaint_service.model import Base
 from complaint_service.logger import logger
+from complaint_service.db_helper import db_helper
 
-# @pytest.fixture
-# async def session():
+@pytest.fixture
+async def session():
+    return db_helper.session_factory
+
+
 #     db_helper = DatabaseHelper(
 #         url="sqlite:///:memory:",
 #     )
