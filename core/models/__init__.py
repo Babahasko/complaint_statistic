c87b4f924a7b48f77cb8901618d9c29af745aa41
Babahasko <zakutni4ek@gmail.com>
2024-10-20 15:20:29 +0300

    create user table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,15 +1,16 @@
 __all__ = (
     "Base",
     "User",
-    "Complain",
-    "Surveillance",
-    "Theme",
-    "user_theme_association_table",
 )
-
+# "Complain",
+# "Surveillance",
+# "Theme",
+# "user_theme_association_table",
+#
 from .base import Base
 from .user import User
-from .complain import Complain
-from .surveillance import Surveillance
-from .theme import Theme
-from .user_theme_association import user_theme_association_table
+
+# # from .complain import Complain
+# # from .surveillance import Surveillance
+# # from .theme import Theme
+# # from .user_theme_association import user_theme_association_table
--- core/models/user.py
+++ core/models/user.py
@@ -1,32 +1,33 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String
-from typing import List
-from typing import TYPE_CHECKING
 
+# from typing import List
+# from typing import TYPE_CHECKING
+#
 from .base import Base
 
-if TYPE_CHECKING:
-    from .surveillance import Surveillance
-    from .complain import Complain
-    from .theme import Theme
-    from .user_theme_association import user_theme_association_table
+# if TYPE_CHECKING:
+#     from .surveillance import Surveillance
+#     from .complain import Complain
+#     from .theme import Theme
+#     from .user_theme_association import user_theme_association_table
 
 
 class User(Base):
     __tablename__ = "user_table"
     id: Mapped[int] = mapped_column(primary_key=True)
     telegramm_account: Mapped[str] = mapped_column(String(30), unique=True)
-    name: Mapped[str] = mapped_column(String(30))
-    surveillance: Mapped[list["Surveillance"]] = relationship(back_populates="user")
-    complains: Mapped[list["Complain"]] = relationship(back_populates="user")
-    themes: Mapped[List["Theme"]] = relationship(
-        secondary=user_theme_association_table,
-        back_populates="users",
-    )
+    username: Mapped[str] = mapped_column(String(30))
+    # surveillance: Mapped[list["Surveillance"]] = relationship(back_populates="user")
+    # complains: Mapped[list["Complain"]] = relationship(back_populates="user")
+    # themes: Mapped[List["Theme"]] = relationship(
+    #     secondary=user_theme_association_table,
+    #     back_populates="users",
+    # )
 
     def __repr__(self):
         return (
             f"User(id={self.id},"
             f" telegramm_account={self.telegramm_account},"
-            f" name={self.name},"
+            f" name={self.username},"
         )
