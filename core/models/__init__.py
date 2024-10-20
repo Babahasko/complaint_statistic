55baf6b9daad8044e1ea30eea2f72c361ae06f5a
Babahasko <zakutni4ek@gmail.com>
2024-10-20 15:45:15 +0300

    create complain table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,19 +2,19 @@ __all__ = (
     "Base",
     "User",
     "Surveillance",
+    "Complain",
 )
 # "Theme",
-# "Complain",
+
 
 # "user_theme_association_table",
 
 from .base import Base
 from .user import User
 from .surveillance import Surveillance
+from .complain import Complain
 
 # from .theme import Theme
-
-# # from .complain import Complain
 
 
 # # from .user_theme_association import user_theme_association_table
