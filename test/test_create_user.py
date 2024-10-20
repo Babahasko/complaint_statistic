caed9d9443ef291eb231823103b21835796fb258
Babahasko <zakutni4ek@gmail.com>
2024-10-20 23:49:04 +0300

    refactor test function

--- test/test_create_user.py
+++ test/test_create_user.py
@@ -7,12 +7,11 @@ from core.utils import logger
 
 
 @pytest.mark.asyncio()
-async def test_create_user():
-    async with db_helper.session_factory() as session:
-        user_schema = UserCreate(username="Bob", telegramm_account="@ponchik")
-        user = await user_crud.create_user(
-            session=session,
-            insert_user=user_schema,
-        )
-        logger.info(f"{user}")
+async def test_create_user(async_session):
+    user_schema = UserCreate(username="Bob", telegramm_account="@ponchik")
+    user = await user_crud.create_user(
+        session=async_session,
+        insert_user=user_schema,
+    )
+    logger.info(f"{user}")
     # logger.info(f"{inserted_user}")
