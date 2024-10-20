b21f7112699d30bff2942045d1122f00f04dc6f4
Babahasko <zakutni4ek@gmail.com>
2024-10-20 15:33:53 +0300

    create surveillance table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,16 +1,20 @@
 __all__ = (
     "Base",
     "User",
+    "Surveillance",
 )
-# "Complain",
-# "Surveillance",
 # "Theme",
+# "Complain",
+
 # "user_theme_association_table",
-#
+
 from .base import Base
 from .user import User
+from .surveillance import Surveillance
+
+# from .theme import Theme
 
 # # from .complain import Complain
-# # from .surveillance import Surveillance
-# # from .theme import Theme
+
+
 # # from .user_theme_association import user_theme_association_table
--- core/models/surveillance.py
+++ core/models/surveillance.py
@@ -1,20 +1,23 @@
-from typing import List
-
+# from typing import List
+#
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, ForeignKey
-from .complain import Complain
-from .user import User
+
+# from .user import User
 
 
+# from .complain import Complain
+#
+#
 class Surveillance(Base):
     __tablename__ = "surveillance_table"
-
     id: Mapped[int] = mapped_column(primary_key=True)
+
     name: Mapped[str] = mapped_column(String(30))
-    complains: Mapped[List["Complain"]] = relationship(back_populates="surveillance")
+    # complains: Mapped[List["Complain"]] = relationship(back_populates="surveillance")
     user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"))
-    user: Mapped["User"] = relationship(back_populates="surveillance")
+    # user: Mapped["User"] = relationship(back_populates="surveillance")
 
     def __repr__(self):
-        return f"Surveillance(id={self.id}, name={self.name}, complains={self.complains}, user={self.user})"
+        return f"Surveillance(name={self.name})"
