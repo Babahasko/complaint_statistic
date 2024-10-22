39c4fa77166b97c6e4c59dd87bbb66d4ccccb104
Babahasko <zakutni4ek@gmail.com>
2024-10-22 23:15:11 +0300

    add username fixture

--- conftest.py
+++ conftest.py
@@ -40,32 +40,57 @@ def complains_factory() -> Callable[[int], Sequence[ComplainCreate]]:
 
 
 @pytest.fixture()
-def telegramm_account_factory() -> Callable[[int], Sequence[str]]:
+def telegramm_account_factory() -> Callable[[], str]:
     prefix_1_list = ["limon", "baskov", "mega", "alpha"]
     prefix_2_list = ["1", "2", "3", "4", "5", "6"]
     prefix_3_list = ["bs", "cs", "kz", "infer", "saber", "giga", "pilo"]
     prefix_4_list = ["kilo", "mili", "anter", "archi", "yandex", "mail", "outlook"]
 
-    def _telegramm_account(num_records: int) -> Sequence[str]:
-        list_telegramm_accounts = []
-        for _ in range(num_records):
-            prefix_1 = random.choice(prefix_1_list)
-            prefix_2 = random.choice(prefix_2_list)
-            prefix_3 = random.choice(prefix_3_list)
-            prefix_4 = random.choice(prefix_4_list)
-            telegramm_account_name = [prefix_1, prefix_2, prefix_3, prefix_4]
-            result = "".join(telegramm_account_name)
-            list_telegramm_accounts.append(result)
-        return list_telegramm_accounts
+    def _telegramm_account() -> str:
+        prefix_1 = random.choice(prefix_1_list)
+        prefix_2 = random.choice(prefix_2_list)
+        prefix_3 = random.choice(prefix_3_list)
+        prefix_4 = random.choice(prefix_4_list)
+        telegramm_account_name = [prefix_1, prefix_2, prefix_3, prefix_4]
+        result = "".join(telegramm_account_name)
+        return result
 
     return _telegramm_account
 
 
-#     db_helper = DatabaseHelper(
-#         url="sqlite:///:memory:",
-#     )
-#     Base.metadata.create_all(db_helper.engine)
-#
-#     yield session
-#     await session.close()
-#     Base.metadata.drop_all(db_helper.engine)
+@pytest.fixture()
+def username_factory() -> Callable[[], str]:
+    username_list = ["Антон", "Гена", "babi4", "Миша", "AlexDark", "Bandos", "Killah"]
+
+    def _username() -> Sequence[str]:
+        return random.choice(username_list)
+
+    return _username
+
+
+@pytest.fixture()
+def theme_factory() -> Callable[[], str]:
+    theme_list = ["Жизнь", "Работа", "Жена", "Машина", "ЖКХ", "Погода", "Начальство"]
+
+    def _theme() -> Sequence[str]:
+        return random.choice(theme_list)
+
+    return _theme
+
+
+@pytest.fixture()
+def surveillance_factory() -> Callable[[], str]:
+    surveillance_list = [
+        "Объект_1",
+        "Начальник",
+        "Жена",
+        "Лёха",
+        "Антон",
+        "Павел",
+        "Гена",
+    ]
+
+    def _surveillance() -> Sequence[str]:
+        return random.choice(surveillance_list)
+
+    return _surveillance
