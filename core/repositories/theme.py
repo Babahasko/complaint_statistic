1980ed9e6c0719df73a65d7f72e3ccbedaa76847
Babahasko <zakutni4ek@gmail.com>
2024-10-23 01:57:05 +0300

    refactor logger.info for theme

--- core/repositories/theme.py
+++ core/repositories/theme.py
@@ -9,6 +9,6 @@ from core.utils import logger
 
 async def create_theme(session: AsyncSession, insert_theme: ThemeCreate) -> Theme:
     theme_dict = insert_theme.model_dump()
-    logger.info(f"theme created {theme_dict}")
     theme = await session.scalars(insert(Theme).returning(Theme), [theme_dict])
+    logger.info(f"theme created {theme.all()}")
     return theme.all()
