cdaa31b8b558957d15626389655a7971e1c88540
Babahasko <zakutni4ek@gmail.com>
2024-10-22 23:17:08 +0300

    import new fixtures

--- test/test_create_user.py
+++ test/test_create_user.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 from core.utils.db_helper import db_helper
 
@@ -10,11 +12,12 @@ from core.repositories import surveillance as surveillance_crud
 
 
 @pytest.mark.asyncio()
-async def test_create_user(async_session, telegramm_account_factory):
-    telegramm_names = telegramm_account_factory(1)
-    logger.info(f"{telegramm_names[0]}")
-    user_schema = UserCreate(username="Gerbert", telegramm_account=telegramm_names[0])
-
+async def test_create_user(async_session, telegramm_account_factory, username_factory):
+    telegramm_account_name = telegramm_account_factory()
+    username = username_factory()
+    user_schema = UserCreate(
+        username=username, telegramm_account=telegramm_account_name
+    )
     user = await user_crud.create_user(
         session=async_session,
         insert_user=user_schema,
@@ -23,21 +26,28 @@ async def test_create_user(async_session, telegramm_account_factory):
 
 
 @pytest.mark.asyncio()
+async def test_select_all_users(async_session):
+    all_users = await user_crud.select_all_users(session=async_session)
+    logger.info(f"{all_users}")
+
+
+@pytest.mark.asyncio()
 async def test_select_user(async_session):
+    all_users = await user_crud.select_all_users(session=async_session)
+    random_user = random.choice(all_users)
     user = await user_crud.select_user_by_username(
-        session=async_session, username="Gerbert"
+        session=async_session, username=random_user.username
     )
     logger.info(user)
 
 
 @pytest.mark.asyncio()
-async def test_create_theme_by_user(async_session):
-
-    user = await user_crud.select_user_by_username(
-        session=async_session, username="Gerbert"
-    )
-    logger.info(user)
-    theme_create = ThemeCreate(name="Mark2 и сжатие пространства", user_id=user.id)
+async def test_create_theme_by_user(async_session, theme_factory):
+    all_users = await user_crud.select_all_users(session=async_session)
+    random_user = random.choice(all_users)
+    logger.info(f"{random_user}")
+    theme_name = theme_factory()
+    theme_create = ThemeCreate(name=theme_name, user_id=random_user.id)
     theme = await theme_crud.create_theme(
         session=async_session, insert_theme=theme_create
     )
@@ -45,12 +55,14 @@ async def test_create_theme_by_user(async_session):
 
 
 @pytest.mark.asyncio()
-async def test_create_surveillance_by_user(async_session):
-    user = await user_crud.select_user_by_username(
-        session=async_session, username="Gerbert"
+async def test_create_surveillance_by_user(async_session, surveillance_factory):
+    all_users = await user_crud.select_all_users(session=async_session)
+    random_user = random.choice(all_users)
+    logger.info(f"{random_user}")
+    surveillance_name = surveillance_factory()
+    surveillance_create = SurveillanceCreate(
+        name=surveillance_name, user_id=random_user.id
     )
-    logger.info(user)
-    surveillance_create = SurveillanceCreate(name="Объект_1", user_id=user.id)
     surveillance = await surveillance_crud.create_surveillance(
         session=async_session, insert_surveillance=surveillance_create
     )
