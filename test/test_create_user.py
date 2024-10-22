7b4aff78f409b340e1604d001ed73ec5c2f7cee9
Babahasko <zakutni4ek@gmail.com>
2024-10-22 22:21:37 +0300

    add test`s for new methods

--- test/test_create_user.py
+++ test/test_create_user.py
@@ -1,17 +1,57 @@
 import pytest
 from core.utils.db_helper import db_helper
 
-from core.schemas import UserCreate
-from core.repositories import user as user_crud
+from core.schemas import UserCreate, ThemeCreate, SurveillanceCreate
 from core.utils import logger
+
+from core.repositories import user as user_crud
+from core.repositories import theme as theme_crud
+from core.repositories import surveillance as surveillance_crud
 
 
 @pytest.mark.asyncio()
-async def test_create_user(async_session):
-    user_schema = UserCreate(username="Bob", telegramm_account="@ponchik")
+async def test_create_user(async_session, telegramm_account_factory):
+    telegramm_names = telegramm_account_factory(1)
+    logger.info(f"{telegramm_names[0]}")
+    user_schema = UserCreate(username="Gerbert", telegramm_account=telegramm_names[0])
+
     user = await user_crud.create_user(
         session=async_session,
         insert_user=user_schema,
     )
     logger.info(f"{user}")
-    # logger.info(f"{inserted_user}")
+
+
+@pytest.mark.asyncio()
+async def test_select_user(async_session):
+    user = await user_crud.select_user_by_username(
+        session=async_session, username="Gerbert"
+    )
+    logger.info(user)
+
+
+@pytest.mark.asyncio()
+async def test_create_theme_by_user(async_session):
+
+    user = await user_crud.select_user_by_username(
+        session=async_session, username="Gerbert"
+    )
+    logger.info(user)
+    theme_create = ThemeCreate(name="Mark2 и сжатие пространства", user_id=user.id)
+    theme = await theme_crud.create_theme(
+        session=async_session, insert_theme=theme_create
+    )
+    logger.info(f"{theme}")
+
+
+@pytest.mark.asyncio()
+async def test_create_surveillance_by_user(async_session):
+    user = await user_crud.select_user_by_username(
+        session=async_session, username="Gerbert"
+    )
+    logger.info(user)
+    surveillance_create = SurveillanceCreate(name="Объект_1", user_id=user.id)
+    surveillance = await surveillance_crud.create_surveillance(
+        session=async_session, insert_surveillance=surveillance_create
+    )
+    logger.info(f"{surveillance}")
