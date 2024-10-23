2d6b39b8fbb1c24113769d6956515c9bc5997d36
Babahasko <zakutni4ek@gmail.com>
2024-10-24 00:51:53 +0300

    select -> get

--- test/help_functions.py
+++ test/help_functions.py
@@ -3,7 +3,7 @@ import random
 from core.utils import logger
 
 
-async def select_random_user(async_session):
+async def get_random_user(async_session):
     all_users = await user_crud.get_all_users(session=async_session)
     random_user = random.choice(all_users)
     logger.info(f"random_user = {random_user}")
