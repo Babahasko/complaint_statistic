06182c3045def1af124bb6aaac5b350e66c5971d
Babahasko <zakutni4ek@gmail.com>
2024-10-23 03:01:41 +0300

    add ComplainCreate

--- core/schemas/__init__.py
+++ core/schemas/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "UserCreate",
     "ThemeCreate",
     "SurveillanceCreate",
+    "ComplainCreate",
 )
 
 from .user import UserCreate
 from .theme import ThemeCreate
 from .surveillance import SurveillanceCreate
+from .complain import ComplainCreate
