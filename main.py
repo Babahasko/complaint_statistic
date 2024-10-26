0b65cafdfff4923f4aebe1a74250ac2fab29591f
Babahasko <zakutni4ek@gmail.com>
2024-10-26 14:28:37 +0300

    make main.py as main app

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
 import asyncio
+import uvicorn
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
 
 
-
-async def main()-> None:
-    await asyncio.sleep(3)
-
-
-# async def main() -> None:
-#     logger.info("Sync db")
-#     engine = current_db.engine
-#     async_session = db_helper.current_db.session_factory
-#     # async with engine.begin() as conn:
-#     #     await conn.run_sync(Base.metadata.create_all)
-# # Пример добавления объекта в бд
-#     example_1 = ComplainCreate(
-#         who='Старый',
-#         about='жизнь',
-#         whom='Даня',
-#         data=datetime.now(),
-#     )
-#     await create_complain(async_session,example_1)
-# # Приме удаления объекта из бд
-#     await delete_complain(async_session, 11)
-# # Пример получения всех записей в БД
-#     result = await get_complains(async_session)
-#     logger.info(f"Result = {result}")
-#
-#     await current_db.engine.dispose()
+class ModelName(Enum):
+    manager = "manager"
+    worker = "worker"
+    service = "service"
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    match model_name:
+        case model_name.manager:
+            return {
+                "model_name": model_name,
+                "message": "Hi dear manager",
+            }
+        case model_name.worker:
+            return {
+                "model_name": model_name,
+                "message": "Hi dear workers",
+            }
+        case model_name.service:
+            return {
+                "model_name": model_name,
+                "message": "Hi dear service",
+            }
+        case _:
+            return {"message": "I don`t know who you are"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": f"current_user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user_by_id(user_id: int):
+    return {"user_id": user_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
