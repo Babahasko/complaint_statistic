9c4ab181d1cbf1e544c72ff9a2db3d73ec8cccf0
Babahasko <zakutni4ek@gmail.com>
2024-10-28 23:07:37 +0300

    make a fastapi app

--- main.py
+++ main.py
@@ -1,57 +1,9 @@
-import asyncio
 import uvicorn
 from fastapi import FastAPI
-from enum import Enum
+from api.router import router as api_router
 
 app = FastAPI()
-
-
-class ModelName(Enum):
-    manager = "manager"
-    worker = "worker"
-    service = "service"
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/model/{model_name}")
-async def get_model(model_name: ModelName):
-    match model_name:
-        case model_name.manager:
-            return {
-                "model_name": model_name,
-                "message": "Hi dear manager",
-            }
-        case model_name.worker:
-            return {
-                "model_name": model_name,
-                "message": "Hi dear workers",
-            }
-        case model_name.service:
-            return {
-                "model_name": model_name,
-                "message": "Hi dear service",
-            }
-        case _:
-            return {"message": "I don`t know who you are"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": f"current_user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user_by_id(user_id: int):
-    return {"user_id": user_id}
+app.include_router(api_router)
 
 
 if __name__ == "__main__":
